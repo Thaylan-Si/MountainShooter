63cc640bafa4f6c227035b1e0038f2d90c04e56a
Thaylan <thaylanti2025@gmail.com>
2026-06-19 19:08:09 -0300

    Changing menu

--- code/menu.py
+++ code/menu.py
@@ -5,7 +5,7 @@ from pygame.font import Font
 from pygame.rect import Rect
 from pygame.surface import Surface
 
-from code.Const import WIN_WIDTH, C_ORANGE, MENU_OPTION, C_WHITE,
+from code.Const import WIN_WIDTH, C_ORANGE, MENU_OPTION, C_WHITE
 
 
 class Menu:
@@ -24,10 +24,7 @@ class Menu:
             self.menu_text(50, "Shooter", C_ORANGE, ((WIN_WIDTH / 2), 120))
 
             for i in range(len(MENU_OPTION)):
-                if i == menu_option:
-                    self.menu_text(20, MENU_OPTION[i], C_YELLOW, ((WIN_WIDTH / 2), 200 + 25 * i))
-                else:
-                    self.menu_text(20, MENU_OPTION[i], C_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
+                self.menu_text(20, MENU_OPTION[i], C_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
             pygame.display.flip()
 
             # Check for all events
